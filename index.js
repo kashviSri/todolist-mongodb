0e77063ed964d200336adff4b01630cc6e1671db
Kashvi Srivastava <kashvi.srivastava1104@gmail.com>
2025-08-20 14:51:12 +0530

    Added project files

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/todoDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const todoSchema = new mongoose.Schema({
+  task: String,
+  priority: { type: String, default: "Normal" },
+  done: { type: Boolean, default: false }
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get('/', async (req, res) => {
+  const filter = req.query.priority || '';
+  const query = filter ? { priority: filter } : {};
+  const todos = await Todo.find(query).lean();
+  res.render("list", {
+    todos,
+    allTodos: todos,
+    error: null,
+    editingIndex: null,
+    selectedPriority: filter
+  });
+});
+
+app.post('/', async (req, res) => {
+  const { task, priority } = req.body;
+  if (!task || task.trim() === "") {
+    const todos = await Todo.find({}).lean();
+    return res.render("list", {
+      todos,
+      allTodos: todos,
+      error: "Task cannot be empty!",
+      editingIndex: null,
+      selectedPriority: ''
+    });
+  }
+  await Todo.create({ task: task.trim(), priority, done: false });
+  res.redirect('/');
+});
+
+app.post('/toggle', async (req, res) => {
+  const index = parseInt(req.body.index);
+  const todos = await Todo.find({}).lean();
+  if (!isNaN(index) && todos[index]) {
+    const todo = todos[index];
+    await Todo.findByIdAndUpdate(todo._id, { done: !todo.done });
+  }
+  res.redirect('/');
+});
+
+app.post('/edit-mode', async (req, res) => {
+  const index = parseInt(req.body.index);
+  const priorityFilter = req.body.filter || '';
+  const query = priorityFilter ? { priority: priorityFilter } : {};
+  const todos = await Todo.find(query).lean();
+  res.render("list", {
+    todos,
+    allTodos: todos,
+    error: null,
+    editingIndex: index,
+    selectedPriority: priorityFilter
+  });
+});
+
+app.post('/edit', async (req, res) => {
+  const { index, updatedTask } = req.body;
+  const i = parseInt(index);
+  const todos = await Todo.find({}).lean();
+  if (!isNaN(i) && updatedTask.trim() && todos[i]) {
+    await Todo.findByIdAndUpdate(todos[i]._id, { task: updatedTask.trim() });
+  }
+  res.redirect('/');
+});
+
+
+app.post('/delete', async (req, res) => {
+  const index = parseInt(req.body.index);
+  const todos = await Todo.find({}).lean();
+  if (!isNaN(index) && todos[index]) {
+    await Todo.findByIdAndDelete(todos[index]._id);
+  }
+  res.redirect('/');
+});
+
+app.listen(8000, () => console.log("Server is running"));
